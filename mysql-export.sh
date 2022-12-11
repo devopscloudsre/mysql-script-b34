7596abd5822db130cbc233f61a3da6c9fea3d622
rahul <mailrahulsre@gmail.com>
2022-12-11 14:40:43 +0000

    shebang added

--- mysql-export.sh
+++ mysql-export.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This is a scriptfor running mysql dump
+
 v_password=$(cat password)
 #v_password=welcome123
 mysqldump -hlwplabs.aws.rds.com -udbuser -p$v_password lwplabs > /tmp/lwplabs_$(date '+%Y-%m-%d-%H:%M:%S').sql
